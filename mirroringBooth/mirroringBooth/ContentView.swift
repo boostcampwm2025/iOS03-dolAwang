9fddbc23ccb00b8b05e9f8dbd7f5476ecc6ada4e
Sang Yu Lee <sangyu857@gmail.com>
2025-12-19 16:15:06 +0900

    chore: 프로젝트 생성 및 초기 설정

---
+++ mirroringBooth/mirroringBooth/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  mirroringBooth
+//
+//  Created by 이상유 on 2025-12-19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ mirroringBooth/mirroringBooth/mirroringBoothApp.swift
@@ -0,0 +1,17 @@
+//
+//  mirroringBoothApp.swift
+//  mirroringBooth
+//
+//  Created by 이상유 on 2025-12-19.
+//
+
+import SwiftUI
+
+@main
+struct mirroringBoothApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
