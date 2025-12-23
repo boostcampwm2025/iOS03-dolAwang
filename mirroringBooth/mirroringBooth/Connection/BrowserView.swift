fdc41743283560762478db95700961d22bc1df06
Sang Yu Lee <sangyu857@gmail.com>
2025-12-24 04:03:10 +0900

    feat: 브라우징 뷰와 연결 매니저 연결 및 정보 바인딩

--- mirroringBooth/mirroringBooth/Connection/BrowserView.swift
+++ mirroringBooth/mirroringBooth/Connection/BrowserView.swift
@@ -8,20 +8,34 @@
 import SwiftUI
 
 struct BrowserView: View {
-    
+
     private var router: Router
-    
+    @State private var connectionManager = ConnectionManager()
+
     init(_ router: Router) {
         self.router = router
     }
-    
+
     var body: some View {
-        Button {
-            router.pop()
-        } label: {
-            Text("뒤로가기")
-                .font(.headline)
-                .padding(5)
+        VStack {
+            Button {
+                router.pop()
+            } label: {
+                Text("뒤로가기")
+                    .font(.headline)
+                    .padding(5)
+            }
+
+            Text(connectionManager.connectionState)
+                .font(.subheadline)
+
+            Text(connectionManager.peers.joined(separator: ", "))
+        }
+        .onAppear {
+            connectionManager.startBrowsing()
+        }
+        .onDisappear {
+            connectionManager.stopBrowsing()
         }
     }
 }
