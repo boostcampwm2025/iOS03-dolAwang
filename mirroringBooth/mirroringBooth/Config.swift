8d4d84fd20f976d057aa5d1dd17857a84997033d
GRJeon <104538652+GRJeon@users.noreply.github.com>
2025-12-29 14:53:11 +0900

    chore: Config 파일 추가

---
+++ mirroringBooth/mirroringBooth/Config.swift
@@ -0,0 +1,11 @@
+//
+//  Config.swift
+//  mirroringBooth
+//
+//  Created by Liam on 12/25/25.
+//
+
+enum Config {
+    // Info.plist Bonjour services에 기재된 내용과 같아야 함.
+    static let serviceType: String = "MirroringBooth"
+}
