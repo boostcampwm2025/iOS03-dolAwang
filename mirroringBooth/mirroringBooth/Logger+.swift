1faaa0d544fd5020b0fb11d0ff00b657a1ac6928
GRJeon <104538652+GRJeon@users.noreply.github.com>
2025-12-29 14:53:22 +0900

    chore: Logger 관리 파일 추가

---
+++ mirroringBooth/mirroringBooth/Logger+.swift
@@ -0,0 +1,15 @@
+//
+//  Logger+.swift
+//  mirroringBooth
+//
+//  Created by Liam on 12/29/25.
+//
+
+import OSLog
+
+extension Logger {
+    private static var subsystem = Bundle.main.bundleIdentifier ?? "kr.codesquad.boostcamp10.dolAwang.mirroringBooth"
+    
+    static let multipeerAdvertiser = Logger(subsystem: subsystem, category: "MultipeerAdvertiser")
+    static let multipeerBrowser = Logger(subsystem: subsystem, category: "MultipeerBrowser")
+}
