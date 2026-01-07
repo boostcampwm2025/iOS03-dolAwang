d8ff45362b07221c040e1f5f424ececba3f8d558
Sang Yu Lee <sangyu857@gmail.com>
2026-01-07 16:39:59 +0900

    design: 카메라 기기의 시작 화면

---
+++ mirroringBooth/mirroringBooth/Device/Camera/CameraHomeView.swift
@@ -0,0 +1,107 @@
+//
+//  CameraHomeView.swift
+//  mirroringBooth
+//
+//  Created by 이상유 on 2026-01-06.
+//
+
+import SwiftUI
+
+struct CameraHomeView: View {
+
+    @Environment(Router.self) var router: Router
+
+    var body: some View {
+        VStack(spacing: 15) {
+            // 1. 타이틀
+            VStack(alignment: .leading) {
+                Group {
+                    Text("가장 선명하게,")
+                    Text("우리다운 순간을")
+                        .foregroundStyle(Color("mainColor"))
+                    Text("기록하다.")
+                }
+                    .font(.title.bold())
+            }
+            .frame(maxWidth: .infinity, alignment: .topLeading)
+
+            // 2. 서비스 이름
+            Text("Mirroring Booth")
+                .font(.caption.bold())
+                .foregroundStyle(Color(.secondaryLabel))
+                .frame(maxWidth: .infinity, alignment: .leading)
+
+            // 3. 기기 용도 선택
+            ScrollView {
+                Button {
+                    router.push(to: CameraRoute.browsing)
+                } label: {
+                    selectionBox(
+                        icons: ["camera"],
+                        title: "촬영 기기로 시작하기",
+                        description: "카메라를 통해 순간을 기록해보세요.",
+                        colors: [Color("mainColor")]
+                    )
+                }
+
+                Button {
+                    // 라우터로 연결 대기 화면으로 이동
+                } label: {
+                    selectionBox(
+                        icons: ["display", "target"],
+                        title: "미러링/리모트 기기로 시작하기",
+                        description: "미러링/리모트 기기로 순간을 공유해보세요.",
+                        colors: [Color("mirroringColor"), Color("remoteColor")]
+                    )
+                }
+            }
+        }
+        .padding()
+    }
+}
+
+extension CameraHomeView {
+    @ViewBuilder
+    private func selectionBox(
+        icons: [String],
+        title: String,
+        description: String,
+        colors: [Color]
+    ) -> some View {
+        VStack {
+            HStack {
+                ForEach(icons.indices, id: \.self) { index in
+                    Image(systemName: icons[index])
+                        .padding(10)
+                        .font(.title.bold())
+                        .foregroundStyle(colors[index])
+                        .background(colors[index].opacity(0.1))
+                        .clipShape(RoundedRectangle(cornerRadius: 15))
+                }
+            }
+
+            Text(title)
+                .font(.title3.bold())
+                .foregroundStyle(Color(.label))
+                .padding(.top)
+
+            Text(description)
+                .font(.footnote)
+                .foregroundStyle(Color(.secondaryLabel))
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .aspectRatio(4/3, contentMode: .fit)
+        .background(Color(.secondarySystemBackground).opacity(0.5))
+        .clipShape(RoundedRectangle(cornerRadius: 15))
+        .overlay {
+            RoundedRectangle(cornerRadius: 15)
+                .stroke(.gray.opacity(0.3), style: StrokeStyle(lineWidth: 1))
+                .shadow(color: .black, radius: 15)
+        }
+        .padding(.horizontal)
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ mirroringBooth/mirroringBooth/Device/Common/AdvertisierHomeView.swift
@@ -0,0 +1,18 @@
+//
+//  AdvertisierHomeView.swift
+//  mirroringBooth
+//
+//  Created by 이상유 on 2026-01-07.
+//
+
+import SwiftUI
+
+struct AdvertisierHomeView: View {
+    var body: some View {
+        Text("미러링/리모트 기기의 시작 화면")
+    }
+}
+
+#Preview {
+    AdvertisierHomeView()
+}
