296279a77bccec0f9454f6d1bf71c9f9367f609d
opficdev <opficdev@gmail.com>
2026-01-07 14:05:52 +0900

    chore: Watch App 기본 개발환경 구성

---
+++ mirroringBooth/mirroringBoothWatch/App/mirroringBoothWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  mirroringBoothWatchApp.swift
+//  mirroringBoothWatch
+//
+//  Created by 최윤진 on 1/7/26.
+//
+
+import SwiftUI
+
+@main
+struct mirroringBoothWatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            RootView()
+        }
+    }
+}
---
+++ mirroringBooth/mirroringBoothWatch/View/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  mirroringBoothWatch Watch App
+//
+//  Created by 최윤진 on 1/7/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
