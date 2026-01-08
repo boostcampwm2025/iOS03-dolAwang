0f5a865cec63628c584d3236b0020bb899e81990
GRJeon <104538652+GRJeon@users.noreply.github.com>
2026-01-08 14:52:48 +0900

    feat: CaptureResultStore

---
+++ mirroringBooth/mirroringBooth/Device/Mirroring/Capture/CaptureResultStore.swift
@@ -0,0 +1,72 @@
+//
+//  CaptureResultStore.swift
+//  mirroringBooth
+//
+//  Created by Liam on 1/7/26.
+//
+
+import Foundation
+
+@Observable
+final class CaptureResultStore: StoreProtocol {
+    struct State {
+        var photos: [Photo] = []
+        var maxSelection: Int
+        var currentSelectionCount: Int = 0
+    }
+
+    enum Intent {
+        case onAppear
+        case selectPhoto(Int)
+    }
+
+    enum Result {
+        case setPhotos([Photo])
+        case selectPhoto(Int)
+        case deselectPhoto(Int)
+        case increaseSelectionCount
+        case decreaseSelectionCount
+    }
+
+    var state: State = .init(maxSelection: 4)
+    let advertiser: Advertisier
+
+    init(advertiser: Advertisier) {
+        self.advertiser = advertiser
+    }
+
+    func action(_ intent: Intent) -> [Result] {
+        switch intent {
+        case .onAppear:
+            return [.setPhotos(advertiser.receivedPhotos)]
+        case let .selectPhoto(index):
+            if state.photos[index].selectNumber == nil {
+                guard state.currentSelectionCount < state.maxSelection else { return [] }
+                return [.selectPhoto(index), .increaseSelectionCount]
+            } else {
+                return [.deselectPhoto(index), .decreaseSelectionCount]
+            }
+        }
+    }
+
+    func reduce(_ result: Result) {
+        switch result {
+        case let .setPhotos(photos):
+            state.photos = photos
+        case let .selectPhoto(index):
+            state.photos[index].selectNumber = state.currentSelectionCount + 1
+        case let .deselectPhoto(index):
+            guard let number = self.state.photos[index].selectNumber else { return }
+            state.photos[index].selectNumber = nil
+            for (index, photo) in self.state.photos.enumerated() {
+                if let iterator = photo.selectNumber, iterator > number {
+                    state.photos[index].selectNumber = iterator - 1
+                }
+            }
+        case .increaseSelectionCount:
+            state.currentSelectionCount += 1
+        case .decreaseSelectionCount:
+            state.currentSelectionCount -= 1
+        }
+    }
+}
