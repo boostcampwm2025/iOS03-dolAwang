17dc37074601b95666a14e7faa46b1e68a55741c
Sang Yu Lee <sangyu857@gmail.com>
2026-01-13 14:32:03 +0900

    design: ResultView

---
+++ mirroringBooth/mirroringBooth/Device/Common/ResultView.swift
@@ -0,0 +1,91 @@
+//
+//  ResultView.swift
+//  mirroringBooth
+//
+//  Created by 이상유 on 2026-01-13.
+//
+
+import SwiftUI
+
+struct ResultView: View {
+    var body: some View {
+        ZStack {
+            Color.background
+                .ignoresSafeArea()
+            
+            VStack(spacing: 30) {
+                /// 홈 버튼
+                HomeButton(
+                    size: .title3) {
+                        // TODO: 홈으로 이동하는 액션
+                    }
+                    .frame(maxWidth: .infinity, alignment: .topLeading)
+                
+                Spacer()
+                
+                /// 완성 텍스트
+                VStack(spacing: 10) {
+                    Text("완성되었습니다!")
+                        .font(.title.bold())
+                    
+                    Text("순간들이 기록되었습니다.")
+                        .font(.caption)
+                        .foregroundStyle(Color(.label).opacity(0.5))
+                }
+                
+                /// 결과 이미지
+                Image(systemName: "photo")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                
+                /// 버튼
+                HStack {
+                    sharingButton(
+                        icon: "square.and.arrow.down",
+                        title: "갤러리 저장",
+                        isContrast: false
+                    ) {
+                        // TODO: 갤러리 저장 액션
+                    }
+                    
+                    sharingButton(
+                        icon: "paperplane",
+                        title: "Airdrop",
+                        isContrast: true
+                    ) {
+                        // TODO: Airdrop 액션
+                    }
+                }
+                //.hidden() // 저장 로직 추가 후 제거
+                
+                Spacer()
+            }
+            .padding()
+            .navigationBarBackButtonHidden()
+        }
+    }
+
+    private func sharingButton(
+        icon: String,
+        title: String,
+        isContrast: Bool,
+        _ action: @escaping () -> Void
+    ) -> some View {
+        Button {
+            action()
+        } label: {
+            HStack {
+                Image(systemName: icon)
+                
+                Text(title)
+            }
+            .padding(.vertical, 15)
+            .padding(.horizontal, 25)
+            .font(.headline)
+            .fontWeight(.heavy)
+            .foregroundStyle(isContrast ? Color(.systemBackground) : Color(.label))
+            .background(isContrast ? Color(.label) : Color(.systemBackground))
+            .clipShape(RoundedRectangle(cornerRadius: 30))
+        }
+    }
+}
