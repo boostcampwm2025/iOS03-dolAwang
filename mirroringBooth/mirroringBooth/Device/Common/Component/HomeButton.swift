a398ace6bd94ff9dc5f6c4654f358fcf373996c6
Sang Yu Lee <sangyu857@gmail.com>
2026-01-13 14:30:50 +0900

    design: HomeButton 컴포넌트

---
+++ mirroringBooth/mirroringBooth/Device/Common/Component/HomeButton.swift
@@ -0,0 +1,38 @@
+//
+//  HomeButton.swift
+//  mirroringBooth
+//
+//  Created by 이상유 on 2026-01-13.
+//
+
+import SwiftUI
+
+struct HomeButton: View {
+    private let size: Font
+    private var action: () -> Void
+
+    init(
+        size: Font,
+        action: @escaping () -> Void
+    ) {
+        self.size = size
+        self.action = action
+    }
+
+    var body: some View {
+        Button {
+            action()
+        } label: {
+            Image(systemName: "house")
+                .padding(5)
+                .font(size)
+                .foregroundStyle(Color(.label))
+                .bold()
+                .overlay {
+                    Circle()
+                        .stroke(Color(.label), lineWidth: 2)
+                }
+        }
+        .foregroundStyle(.clear)
+    }
+}
