2ba4cc4b3793fb853f72a2088960c1bc96ba03ed
GRJeon <104538652+GRJeon@users.noreply.github.com>
2026-01-14 20:23:16 +0900

    feat: LocalAsyncImage 뷰

---
+++ mirroringBooth/mirroringBooth/Device/Mirroring/Capture/View/CaptureResultComponents/LocalAsyncImage.swift
@@ -0,0 +1,44 @@
+//
+//  LocalAsyncImage.swift
+//  mirroringBooth
+//
+//  Created by Liam on 1/14/26.
+//
+
+import SwiftUI
+
+struct LocalAsyncImage: View {
+    let url: URL
+    @State private var image: Image?
+
+    var body: some View {
+        Group {
+            if let image = image {
+                image
+                    .resizable()
+                    .scaledToFit()
+            } else {
+                Color.gray.opacity(0.3)
+            }
+        }
+        .task {
+            await loadImage()
+        }
+    }
+
+    private func loadImage() async {
+        guard image == nil else { return }
+
+        let loadedImage = await Task.detached(priority: .userInitiated) {
+            UIImage(contentsOfFile: url.path(percentEncoded: false))
+        }.value
+
+        if let loadedImage {
+            withAnimation {
+                self.image = Image(uiImage: loadedImage)
+            }
+        } else {
+            self.image = Image(systemName: "exclamationmark.triangle")
+        }
+    }
+}
