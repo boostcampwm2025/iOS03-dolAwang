2b1b0f90beae89da83565d7bc3c2c0d15185ba73
YunDaeHyeon <kayuaao96@gmail.com>
2026-01-14 16:24:22 +0900

    refactor: StandbyView 공용 컴포넌트화

--- mirroringBooth/mirroringBooth/Device/Common/Advertiser/AdvertiserHomeView.swift
+++ mirroringBooth/mirroringBooth/Device/Common/Advertiser/AdvertiserHomeView.swift
@@ -54,45 +54,6 @@ struct AdvertiserHomeView: View {
     }
 }
 
-// MARK: - Components
-
-private struct StandbyView: View {
-    let displayName: String
-    let isAdvertising: Bool
-
-    @State private var spin = false
-
-    var body: some View {
-        VStack(spacing: 6) {
-            Group {
-                Image(systemName: "arrow.2.circlepath")
-                    .font(.title2)
-                    .rotationEffect(.degrees(-45))
-                    .rotationEffect(.degrees(spin ? 360 : 0))
-                    .animation(
-                        .linear(duration: 1).repeatForever(autoreverses: false),
-                        value: spin
-                    )
-                    .onAppear {
-                        // "상태 변화"를 만들어 repeatForever 트리거
-                        if isAdvertising { spin = true }
-                    }
-                    .onChange(of: isAdvertising) { _, newValue in
-                        spin = newValue
-                    }
-
-                Text("연결 대기 중...")
-                    .fontWeight(.heavy)
-            }
-            .foregroundStyle(Color(.lightGray))
-
-            Text("\(displayName)으로 검색되는 중입니다.")
-                .font(.caption2.bold())
-                .foregroundStyle(Color(.darkGray))
-        }
-    }
-}
-
 // 작업 이전 뷰라서 Preview를 제거하지 않은 상태입니다
 #Preview {
     AdvertiserHomeView()
