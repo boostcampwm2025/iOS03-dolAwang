e5961ce9fd84fcf3cf5404cfc2504295b659a2f5
YunDaeHyeon <kayuaao96@gmail.com>
2026-01-14 16:49:32 +0900

    feat: 미러링기기 재연결 UI 초안

---
+++ mirroringBooth/mirroringBooth/Device/Common/Advertiser/AdvertiserReconnectionView.swift
@@ -0,0 +1,63 @@
+//
+//  AdvertiserReconnectionView.swift
+//  mirroringBooth
+//
+//  Created by 윤대현 on 1/14/26.
+//
+
+import SwiftUI
+
+struct AdvertiserReconnectionView: View {
+    let store: AdvertiserHomeStore
+
+    var body: some View {
+        VStack(spacing: 24) {
+            VStack(spacing: 8) {
+                Image(systemName: "exclamationmark.triangle.fill")
+                    .padding(15)
+                    .font(.title.bold())
+                    .foregroundStyle(.red)
+                    .background(.red.opacity(0.2))
+                    .clipShape(Capsule())
+                Text("촬영 기기와의 연결이 끊어졌습니다.")
+                    .font(.title2.bold())
+                Text("다시 연결해주세요.")
+                    .font(.footnote)
+                    .foregroundStyle(Color(.secondaryLabel))
+            }
+
+            if store.state.isAdvertising {
+                StandbyView(
+                    displayName: store.advertiser.myDeviceName,
+                    isAdvertising: store.state.isAdvertising
+                )
+            } else {
+                IdleView(
+                    displayName: store.advertiser.myDeviceName
+                )
+            }
+
+            Button {
+                withAnimation(.easeInOut(duration: 0.2)) {
+                    store.send(.didTapAdvertiseButton)
+                }
+            } label: {
+                AdvertisingButton(isAdvertising: store.state.isAdvertising)
+            }
+        }
+        .onAppear {
+            store.send(.didTapAdvertiseButton)
+        }
+        .onDisappear {
+            store.send(.exit)
+        }
+    }
+}
+
+#Preview {
+    AdvertiserReconnectionView(
+        store: AdvertiserHomeStore(
+            Advertiser(photoCacheManager: PhotoCacheManager.shared)
+        )
+    )
+}
