7f3834cb1fc017021b1c75c26406b898faeb2c3e
Sang Yu Lee <sangyu857@gmail.com>
2026-01-15 21:09:30 +0900

    design: AdvertiserReconnectionView에 홈 버튼 추가

--- mirroringBooth/mirroringBooth/Device/Common/Advertiser/AdvertiserReconnectionView.swift
+++ mirroringBooth/mirroringBooth/Device/Common/Advertiser/AdvertiserReconnectionView.swift
@@ -14,23 +14,31 @@ struct AdvertiserReconnectionView: View {
     @State private var isRotating = false
 
     var body: some View {
-        VStack(spacing: 16) {
-            VStack(spacing: 8) {
-                Image(systemName: "exclamationmark.triangle.fill")
-                    .padding(15)
-                    .font(.title.bold())
-                    .foregroundStyle(.red)
-                    .background(.red.opacity(0.2))
-                    .clipShape(Capsule())
-                Text("촬영 기기와의 연결이 끊어졌습니다.")
-                    .font(.title2.bold())
-                Group {
-                    Text("재연결을 시도합니다.")
-                        .font(.footnote)
+        VStack(spacing: 8) {
+            Image(systemName: "exclamationmark.triangle.fill")
+                .padding(15)
+                .font(.title.bold())
+                .foregroundStyle(.red)
+                .background(.red.opacity(0.2))
+                .clipShape(Capsule())
+            Text("촬영 기기와의 연결이 끊어졌습니다.")
+                .font(.title2.bold())
+            Group {
+                Text("재연결을 시도합니다.")
+                    .font(.footnote)
 
-                    SpinningIndicatorView(isActive: true)
-                }
-                .foregroundStyle(Color(.secondaryLabel))
+                SpinningIndicatorView(isActive: true)
+            }
+            .foregroundStyle(Color(.secondaryLabel))
+
+            Button {
+                store.reduce(.setIsReconnectRequired(false))
+                onMoveToHome()
+            } label: {
+                Text("첫 화면으로 돌아가기")
+                    .font(.footnote)
+                    .foregroundStyle(Color(.label))
+                    .opacity(0.8)
             }
         }
         .padding(16)
