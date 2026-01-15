a52483744c6bb41bc2d3a96e5b4cf459f5d93f64
Sang Yu Lee <sangyu857@gmail.com>
2026-01-15 17:59:09 +0900

    feat: 연결 중이던 리모트 기기가 워치일 경우 워치에게 연결 해제 요청

--- mirroringBooth/mirroringBooth/Device/Camera/Connection/BrowsingStore.swift
+++ mirroringBooth/mirroringBooth/Device/Camera/Connection/BrowsingStore.swift
@@ -84,6 +84,10 @@ final class BrowsingStore: StoreProtocol {
         }
 
         browser.onResetRemoteDeviceCommand = { [weak self] in
+            // 리모트 기기가 워치인 경우 워치에게 연결 해제 알림
+            if self?.state.remoteDevice?.type == .watch {
+                self?.watchConnectionManager.sendDisconnectionNotification()
+            }
             self?.reduce(.setRemoteDevice(nil))
         }
     }
