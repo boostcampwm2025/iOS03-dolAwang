b4be5fb637b7f72da861e3a80056beb611808dc8
Sang Yu Lee <sangyu857@gmail.com>
2026-01-22 00:09:55 +0900

    design: HomeView가 아이패드에도 대응하도록 UI 수정

--- mirroringBooth/mirroringBooth/Device/Common/HomeView.swift
+++ mirroringBooth/mirroringBooth/Device/Common/HomeView.swift
@@ -16,33 +16,53 @@ struct HomeView: View {
             MainHeaderView()
 
             Spacer()
-            VStack {
-                Button {
-                    router.push(to: CameraRoute.browsing)
-                } label: {
-                    selectionBox(
-                        icons: ["camera"],
-                        title: "촬영 기기로 시작하기",
-                        description: "카메라를 통해 순간을 기록해보세요.",
-                        colors: [Color.main]
-                    )
-                }
 
-                Button {
-                    router.push(to: CameraRoute.advertising)
-                } label: {
-                    selectionBox(
-                        icons: ["display", "target"],
-                        title: "미러링/리모트 기기로 시작하기",
-                        description: "Apple 기기로 순간을 공유해보세요.",
-                        colors: [Color.mirroring, Color.remote]
-                    )
+            GeometryReader { geometry in
+                if geometry.size.width > geometry.size.height {
+                    HStack {
+                        startButtons(isPortrait: false)
+                    }
+                    .frame(maxWidth: .infinity, alignment: .center)
+                } else {
+                    VStack(spacing: 16) {
+                        startButtons(isPortrait: true)
+                    }
+                    .frame(maxWidth: .infinity, alignment: .center)
+                    .padding(.vertical, 30)
                 }
             }
+
             Spacer()
         }
         .padding(.horizontal)
         .backgroundStyle()
+    }
+
+    @ViewBuilder
+    private func startButtons(isPortrait: Bool) -> some View {
+        Button {
+            router.push(to: CameraRoute.browsing)
+        } label: {
+            selectionBox(
+                icons: ["camera"],
+                title: "촬영 기기로 시작하기",
+                description: "카메라를 통해 순간을 기록해보세요.",
+                colors: [Color.main],
+                isPortrait: isPortrait
+            )
+        }
+
+        Button {
+            router.push(to: CameraRoute.advertising)
+        } label: {
+            selectionBox(
+                icons: ["display", "target"],
+                title: "미러링/리모트 기기로 시작하기",
+                description: "Apple 기기로 순간을 공유해보세요.",
+                colors: [Color.mirroring, Color.remote],
+                isPortrait: isPortrait
+            )
+        }
     }
 }
 
@@ -52,7 +72,8 @@ extension HomeView {
         icons: [String],
         title: String,
         description: String,
-        colors: [Color]
+        colors: [Color],
+        isPortrait: Bool
     ) -> some View {
         VStack {
             HStack {
@@ -76,7 +97,7 @@ extension HomeView {
                 .foregroundStyle(Color(.secondaryLabel))
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity)
-        .aspectRatio(4/3, contentMode: .fit)
+        .aspectRatio(isPortrait ? 4/3 : 3/4, contentMode: .fit)
         .background(Color(.secondarySystemBackground).opacity(0.5))
         .clipShape(RoundedRectangle(cornerRadius: 15))
         .overlay {
