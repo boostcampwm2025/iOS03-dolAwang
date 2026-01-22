92a8fb7f479d933ad581d55c0454af4e5ceb7764
GRJeon <104538652+GRJeon@users.noreply.github.com>
2026-01-22 11:25:47 +0900

    refactor: deinit 콜백 해제

--- mirroringBooth/mirroringBooth/Device/SubDevice/Advertiser/AdvertisingView.swift
+++ mirroringBooth/mirroringBooth/Device/SubDevice/Advertiser/AdvertisingView.swift
@@ -61,6 +61,7 @@ struct AdvertisingView: View {
         }
         .onDisappear {
             store.send(.exit)
+            store.advertiser.onHeartBeatTimeout = nil
         }
         .onChange(of: store.state.hasConnectionStarted) { _, newValue in
             if newValue {
