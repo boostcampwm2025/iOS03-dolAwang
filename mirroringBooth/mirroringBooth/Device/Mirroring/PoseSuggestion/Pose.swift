02309bdeae847868ec2d26e5f8ee13ef3d51b20a
Sang Yu Lee <sangyu857@gmail.com>
2026-01-26 21:36:37 +0900

    add: 포즈 관련 데이터 구조

---
+++ mirroringBooth/mirroringBooth/Device/Mirroring/PoseSuggestion/Pose.swift
@@ -0,0 +1,31 @@
+//
+//  Pose.swift
+//  mirroringBooth
+//
+//  Created by 이상유 on 2026-01-26.
+//
+
+enum Pose: String, CaseIterable, Hashable {
+    case smile
+    case wink
+    case pout
+    case serious
+    case playfulGlance
+    case cry
+    case salute
+    case thinking
+    case neutral
+    case shh
+    case yawn
+    case peace
+    case handHeart
+    case thumbsUp
+    case fistBump
+    case fingerHeart
+    case catPose
+    case tigerPose
+    case pigNose
+    case flowerPose
+    case puffCheeks
+    case takePhoto
+}
---
+++ mirroringBooth/mirroringBooth/Device/Mirroring/PoseSuggestion/PoseMeta.swift
@@ -0,0 +1,11 @@
+//
+//  PoseMeta.swift
+//  mirroringBooth
+//
+//  Created by 이상유 on 2026-01-26.
+//
+
+struct PoseMeta: Decodable, Hashable {
+    let emoji: String
+    let text: String
+}
