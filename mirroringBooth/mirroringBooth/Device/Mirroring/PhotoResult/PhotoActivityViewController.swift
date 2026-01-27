f771bc5743bd6b611c2ff953dd15668a7882ac39
YunDaeHyeon <kayuaao96@gmail.com>
2026-01-27 11:49:19 +0900

    feat: 공유 시트 Preview 구현을 위한 UIActivityItemSource 추가

--- mirroringBooth/mirroringBooth/Device/Mirroring/PhotoResult/PhotoActivityViewController.swift
+++ mirroringBooth/mirroringBooth/Device/Mirroring/PhotoResult/PhotoActivityViewController.swift
@@ -6,15 +6,22 @@
 //
 
 import SwiftUI
+import LinkPresentation
 
+// MARK: - 공유 시트 담당 ActivityViewController
 struct PhotoActivityViewController: UIViewControllerRepresentable {
-    let activityItems: [Any]
+    let image: UIImage
     let applecationActivities: [UIActivity]?
     let excludedActivityTypes: [UIActivity.ActivityType]?
 
     func makeUIViewController(context: Context) -> UIActivityViewController {
+        let appIcon = UIImage(named: "AppIcon") ?? UIImage(systemName: "camera.fill")
+
+        // ActivityItemSource 연결
+        let itemSource = PhotoActivityItemSource(image: image, appIcon: appIcon)
+
         let controller = UIActivityViewController(
-            activityItems: activityItems,
+            activityItems: [itemSource],
             applicationActivities: applecationActivities
         )
 
@@ -25,4 +32,39 @@ struct PhotoActivityViewController: UIViewControllerRepresentable {
     }
 
     func updateUIViewController(_ uiViewController: UIActivityViewController, context: Context) { }
+}
+
+// MARK: - ActivityItemSource (ActivityViewController에 연결되는 프리뷰 인스턴스)
+class PhotoActivityItemSource: NSObject, UIActivityItemSource {
+    let image: UIImage
+    let appIcon: UIImage?
+
+    init(image: UIImage, appIcon: UIImage?) {
+        self.image = image
+        self.appIcon = appIcon
+        super.init()
+    }
+
+    func activityViewControllerPlaceholderItem(_ activityViewController: UIActivityViewController) -> Any {
+        return image
+    }
+
+    func activityViewController(_ activityViewController: UIActivityViewController, itemForActivityType activityType: UIActivity.ActivityType?) -> Any? {
+        return image
+    }
+
+    func activityViewControllerLinkMetadata(_ activityViewController: UIActivityViewController) -> LPLinkMetadata? {
+        let metadata = LPLinkMetadata()
+        metadata.title = "Mirroring Booth"
+
+        // 앱 아이콘 설정
+        if let appIcon = appIcon {
+            metadata.iconProvider = NSItemProvider(object: appIcon)
+        }
+
+        // 이미지 프리뷰 설정
+        metadata.imageProvider = NSItemProvider(object: image)
+
+        return metadata
+    }
 }
