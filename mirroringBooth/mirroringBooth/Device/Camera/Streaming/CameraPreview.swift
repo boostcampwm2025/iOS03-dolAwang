dc58e8ad71ffd2d8d4c06f777b49a05376022112
GRJeon <104538652+GRJeon@users.noreply.github.com>
2026-01-28 16:25:41 +0900

    fix: camera session 종료 조건 수정

--- mirroringBooth/mirroringBooth/Device/Camera/Streaming/CameraPreview.swift
+++ mirroringBooth/mirroringBooth/Device/Camera/Streaming/CameraPreview.swift
@@ -78,6 +78,9 @@ struct CameraPreview: View {
                 rootStore?.browser?.sendCommand(.switchSelectModeView)
             }
         }
+        .onDisappear {
+            store.send(.stopCameraSession)
+        }
         .onChange(of: UIDevice.current.orientation.rawValue) { _, value in
             withAnimation(.easeInOut(duration: 0.3)) {
                 store.send(.updateAngle(rawValue: value))
@@ -97,7 +100,6 @@ struct CameraPreview: View {
             message: "기기 연결이 끊겼습니다.",
             cancellable: false
         ) {
-            store.send(.stopCameraSession)
             dismiss()
             router.reset()
         }
