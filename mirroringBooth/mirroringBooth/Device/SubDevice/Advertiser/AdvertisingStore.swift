8901d8398df0eb97af2d2bbfd11cb5cb2db154bb
Sang Yu Lee <sangyu857@gmail.com>
2026-02-03 01:21:35 +0900

    refactor: State~state 선언부까지 순서 정리

--- mirroringBooth/mirroringBooth/Device/SubDevice/Advertiser/AdvertisingStore.swift
+++ mirroringBooth/mirroringBooth/Device/SubDevice/Advertiser/AdvertisingStore.swift
@@ -19,10 +19,10 @@ final class AdvertisingStore: StoreProtocol {
     }
 
     enum Intent {
-        case onAppear
-        case connected
+        case entry
         case exit
         case setShowTutorial(Bool)
+        case connected
     }
 
     enum Result {
@@ -43,18 +43,18 @@ final class AdvertisingStore: StoreProtocol {
 
     func action(_ intent: Intent) -> [Result] {
         switch intent {
-        case .onAppear:
+        case .entry:
             advertiser.startSearching()
             return [.setOnNavigate(false, type: nil)]
-
-        case .connected:
-            advertiser.stopSearching(onlyRefuse: true)
-            return [.setIsConnected(true)]
 
         case .exit:
             commandTask?.cancel()
             advertiser.stopSearching()
             return []
+
+        case .connected:
+            advertiser.stopSearching(onlyRefuse: true)
+            return [.setIsConnected(true)]
 
         case .setShowTutorial(let value):
             return [.setShowTutorial(value)]
