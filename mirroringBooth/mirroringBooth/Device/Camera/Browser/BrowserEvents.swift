f70f0ac26b4c93e626d0468a34c68f65102599e4
YunDaeHyeon <kayuaao96@gmail.com>
2026-02-02 17:36:54 +0900

    chore: Lint 경고 제거

--- mirroringBooth/mirroringBooth/Device/Camera/Browser/BrowserEvents.swift
+++ mirroringBooth/mirroringBooth/Device/Camera/Browser/BrowserEvents.swift
@@ -14,15 +14,15 @@ enum BrowserEvents {
     case deviceLost(NearbyDevice)
     case deviceConnected(NearbyDevice)
     case deviceConnectionFailed
-    
+
     // 카메라
     case captureCommand
     case sendPhoto
-    
+
     // 리모트 모드
     case remoteModeCommand
     case selectedTimerModeCommand
-    
+
     // Heartbeat
     case heartbeatTimeout
     case remoteHeartbeatTimeout
