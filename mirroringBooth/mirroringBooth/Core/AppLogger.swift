d7c99af98160a463de946d1f875a5223ec306344
GRJeon <104538652+GRJeon@users.noreply.github.com>
2026-02-03 16:14:43 +0900

    refactor: AdvertiserCommandManager 객체

--- mirroringBooth/mirroringBooth/Core/AppLogger.swift
+++ mirroringBooth/mirroringBooth/Core/AppLogger.swift
@@ -14,6 +14,7 @@ extension Logger {
 
     static let advertiser = Logger(subsystem: subsystem, category: "Advertiser")
     static let advertisingManager = Logger(subsystem: subsystem, category: "AdvertisingManager")
+    static let advertiserCommandmanager = Logger(subsystem: subsystem, category: "AdvertiserCommandmanager")
     static let browser = Logger(subsystem: subsystem, category: "Browser")
 
     static let h264encoder = Logger(subsystem: subsystem, category: "H264Encoder")
