662247d28b6fe3e4ac67aac9ed2f561ed812d6ec
YunDaeHyeon <kayuaao96@gmail.com>
2026-02-04 21:52:13 +0900

    refactor: Browser 내부 enum Command 분리
    - 기존 Browser.MirroringDeviceCommand, Browser.RemoteDeviceCommand 사용처에서 Browser. 만 제거
    - BrowserManager 폴더 생성 및 관련 파일 이동

---
+++ mirroringBooth/mirroringBooth/Device/Camera/Browser/BrowserManager/BrowserCommands.swift
@@ -0,0 +1,25 @@
+//
+//  BrowserCommands.swift
+//  mirroringBooth
+//
+//  Created by 윤대현 on 2/4/26.
+//
+
+enum MirroringDeviceCommand: String {
+    case navigateToSelectModeWithRemote
+    case navigateToSelectModeWithoutRemote
+    case switchSelectModeView
+    case onStoreAllPhotos // 사진 10장 저장 시작 명령(from camera)
+    case onUpdateCaptureCount // 리모트 기기에서 카메라 캡처 요청 보내기
+    case heartBeat
+    case captureEffect
+}
+
+enum RemoteDeviceCommand: String {
+    case navigateToRemoteCapture
+    case navigateToRemoteComplete
+    case navigateToRemoteConnected
+    case navigateToHome
+    case noticeIsRemoteDevice
+    case heartBeat
+}
